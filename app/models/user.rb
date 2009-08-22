5320659894c5c28d3020df4e2200ff44bf0a853b
Shaliko Usubov <shaliko@ezid.ru>
2009-08-22 08:38:28 +0400

    Add forgot password functional

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   acts_as_authentic do |c|
+    c.login_field            = :email
     c.openid_required_fields = [:nickname, :email]
+  end
+
+  def deliver_password_reset_instructions!  
+    reset_perishable_token!
+    Notifier.deliver_password_reset_instructions(self)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 ActionController::Routing::Routes.draw do |map|
   map.login "login",    :controller => "user_sessions", :action => "new"
   map.logout "logout",  :controller => "user_sessions", :action => "destroy"
+  map.sign_up 'signup', :controller => "users", :action => "new"
+  map.reset_password  "reset_password", :controller => "password_resets", :action => "new"
 
   map.resources :user_sessions
 
   map.resources :users
+
+  map.resources :password_resets
 
   #static pages
   map.about '/about', :controller => 'static_pages', :action => 'about'
